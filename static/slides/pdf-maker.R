ab85b824df8720b63b6a8bfbd880a95537dc60c1
joshuafayallen <jallen108@gsu.edu>
2022-08-19 11:35:39 -0400

    add ggplot presentation

--- static/slides/pdf-maker.R
+++ static/slides/pdf-maker.R
@@ -1,2 +1,11 @@
 renderthis::to_pdf(from = "Getting-Started-in-R.html", to = "Getting-Started-in-R.pdf",
                    complex_slides = TRUE, partial_slides = TRUE)
+
+
+
+renderthis::to_pdf(from = "data-cleaning.html", to = "data-cleaning.pdf",
+                   complex_slides = TRUE, partial_slides = TRUE)
+
+
+renderthis::to_pdf(from = "ggplot-presentation.html", to = "ggplot-presentation.pdf",
+                   complex_slides = TRUE, partial_slides = TRUE)
